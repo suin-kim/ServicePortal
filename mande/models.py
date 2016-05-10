0bb7bc91236ae60c0231af32c43a0583ae463404
Suin Kim <suinkim98@gmail.com>
2016-05-10 14:19:53 +0700

    idk what i changed

--- mande/models.py
+++ mande/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from datetime import datetime
 
-
+TYPES = (
+    (1,'Babysitting'),
+    (2,'IT'),
+    (3,'Ushering')
+)
 class Opportunity(models.Model):
     opportunity_id = models.AutoField(primary_key=True)
-    opportunity_title = models.CharField(max_length=200)
+    opportunity_title = models.CharField(max_length=20)
     opportunity_details = models.CharField(max_length=200)
+    image = models.IntegerField('Opportunity Type',max_length=1,choices=TYPES,default=1)
     opportunity_requirements = models.CharField(max_length=200, default = "None")
     hours_earned = models.PositiveIntegerField(default=1)
     email = models.EmailField(max_length=100)
     date = models.DateTimeField('date')
-    image = models.ImageField(upload_to= 'static/mande/uploads/', blank=True)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     class Meta:
